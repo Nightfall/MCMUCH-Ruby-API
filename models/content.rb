f77dc6a53a5a0b221f3fca5eeed10b20856fb976
RX14 <chris@rx14.co.uk>
2015-08-28 14:34:03 +0100

    Add validations

--- models/content.rb
+++ models/content.rb
@@ -3,4 +3,9 @@ class Content < ActiveRecord::Base
     self.primary_key = "sha1"
 
     belongs_to :user
+
+    validates :sha1, presence: true
+
+    validates :name, presence: true
+    validates :type, presence: true
 end
--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
     has_many :content
+
+    validates :username, presence: true
+    validates :email, presence: true, with: /@/
+    validates :password_hash, presence:true
 end
