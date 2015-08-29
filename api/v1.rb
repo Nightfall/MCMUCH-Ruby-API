376fb13e102e083e83c715f5d273ace973280112
Striking <Strikingwolf2012@gmail.com>
2015-08-28 19:00:38 -0500

    Some basic api kinda stuff. Please critique @RX14 and @trajing

--- api/v1.rb
+++ api/v1.rb
@@ -2,24 +2,44 @@ class APIv1 < Sinatra::Base
     register Sinatra::ActiveRecordExtension
 
     configure :development do
-        use BetterErrors::Middleware
-        BetterErrors.application_root = __dir__
+      use BetterErrors::Middleware
+      BetterErrors.application_root = __dir__
 
-        register Sinatra::Reloader
+      register Sinatra::Reloader
     end
 
     before do
-        request.body.rewind
-        @body = request.body.read
-        @json = JSON.parse @body if @body.size > 0
+      request.body.rewind
+      @body = request.body.read
+      @json = JSON.parse @body if @body.size > 0
+    end
+
+    get "/content/name=:name" do |name|
+      Content.where(name: name).to_json
+    end
+
+
+    get "/content/user=:user" do |user|
+      Content.where(user: user).to_json
     end
 
     get "/content" do
-        Content.all.to_json
+      Content.all.to_json
     end
 
     put "/content" do
-        c = Content.new @json
-        c.save!
+      Content.new(@json).save!
+    end
+
+    get "/user" do
+      User.all.to_json
+    end
+
+    get "/user/:id" do |id|
+      User.find_by(id: id.to_i).to_json
+    end
+
+    put "/user" do
+      User.new(@json).save!
     end
 end
--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
-    has_many :content
+  has_many :content
 
-    validates :username, presence: true
-    validates :email, presence: true, with: /@/
-    validates :password_hash, presence:true
+  validates :uuid_hash, presence: true
 end
