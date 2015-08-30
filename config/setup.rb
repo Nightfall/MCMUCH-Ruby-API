7b447d856fd8e7fe279ec4841e5d376489f24c80
RX14 <chris@rx14.co.uk>
2015-08-30 17:09:00 +0100

    Implement mojang hex digest

--- config/setup.rb
+++ config/setup.rb
@@ -7,5 +7,5 @@ Bundler.require(:default, ENV["RACK_ENV"])
 Sinatra::Base.set :database_file, __dir__ + "/../config/database.yml"
 
 require_all __dir__ + "/../models"
-# require_all __dir__ + "/../helpers"
+require_all __dir__ + "/../helpers"
 require_all __dir__ + "/../api"
---
+++ helpers/minecraft_digest.rb
@@ -0,0 +1,31 @@
+require "digest"
+
+class MinecraftDigest
+  def self.java_hex_digest(data)
+    hash = Digest::SHA1.digest(data).bytes
+
+    negative = (hash[0] & 0x80) == 0x80
+
+    twos_compliment(hash) if negative
+
+    string = hash.map { |b| sprintf("%02x", b)[-2..-1] }.join.sub(/^0+/, "")
+
+    string = "-" + string if negative
+
+    string
+  end
+
+  def self.twos_compliment(hash)
+    carry = true
+    (hash.length - 1).step(0, -1) do |i|
+      hash[i] = ~hash[i]
+      if carry
+        carry = hash[i] == 0xFF
+        hash[i] += 1
+      end
+    end
+  end
+end
+
+
+
